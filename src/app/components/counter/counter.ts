78a9c2f4f4187e43d9d380f10abac5812e4018f1
AndresN329 <andresnieblesmartinez@gmail.com>
2026-01-17 15:49:34 -0500

    feat: implement parent-child communication with Input and Output (day 3)

--- src/app/components/counter/counter.ts
+++ src/app/components/counter/counter.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
+  standalone: true,
   selector: 'app-counter',
   imports: [],
   templateUrl: './counter.html',
   styleUrl: './counter.css',
 })
-export class Counter {
+export class CounterComponent {
+  @Input() value = 0;
+  @Output() valueChange = new EventEmitter<number>();
 
+  increment() { 
+    this.valueChange.emit(this.value + 1); 
+  }
 }
