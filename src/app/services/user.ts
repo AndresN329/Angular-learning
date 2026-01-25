ae82acf47c325a51ffadcc7e1f1108f25b29f24b
AndresN329 <andresnieblesmartinez@gmail.com>
2026-01-25 17:47:27 -0500

    feat: add user service with ui states (day 5)

---
+++ src/app/services/user.ts
@@ -0,0 +1,31 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable, signal } from '@angular/core';
+import { USERS_MOCK } from '../mocks/users.mock';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserService {
+  
+  // holds the list of users
+  users = signal<any[]>([]); 
+
+  // indicates loading state for the UI
+  loading = signal(false);
+
+  error = signal<string | null>(null); // stores error message if the request fails
+
+  constructor(private http: HttpClient) {}// HttpClient injected for future real APIs
+  
+  // loads users (mocked for now, simulating an API call)
+  loadUsers() {
+    this.loading.set(true);
+    this.error.set(null);
+
+  // simulate backend response
+  setTimeout(() => {
+    this.users.set(USERS_MOCK);
+    this.loading.set(false);
+  }, 800);
+  }
+}
