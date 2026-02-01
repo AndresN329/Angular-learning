938cd2d17ba1ab5d869136c50495243a9bb6855a
AndresN329 <andresnieblesmartinez@gmail.com>
2026-01-31 22:38:15 -0500

    feat(day-10): add template-driven login form with validation

---
+++ src/app/pages/login/login.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AuthService } from '../../core/services/auth.service';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './login.html',
+  styleUrl: './login.css',
+})
+export class LoginComponent {
+  username = '';
+  password = '';
+
+  constructor(private auth: AuthService) {}
+
+  login() {
+    if (this.username && this.password) {
+      this.auth.login();
+    }
+  }
+}
