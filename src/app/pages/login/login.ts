3d92f0e5db589fba027e1da6d03dbfc6a8f63e63
AndresN329 <andresnieblesmartinez@gmail.com>
2026-02-01 19:29:02 -0500

    reafactor(day-11): migrate login component to reactive forms

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,22 +1,29 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
 import { AuthService } from '../../core/services/auth.service';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class LoginComponent {
-  username = '';
-  password = '';
+  form;
 
-  constructor(private auth: AuthService) {}
+  constructor(
+    private fb: FormBuilder,
+    private auth: AuthService
+  ) {
+    this.form = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(4)]],
+    });
+  }
 
   login() {
-    if (this.username && this.password) {
+    if (this.form.valid) {
       this.auth.login();
     }
   }
