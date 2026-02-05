39f1b9096d49d648d8d5b336731c00331e6ec5fd
AndresN329 <andresnieblesmartinez@gmail.com>
2026-02-04 21:19:27 -0500

    chore(day-14): register http interceptors in  app conffig

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,13 +4,17 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { loggingInterceptor } from './core/interceptors/logging.interceptor';
+import { errorInterceptor } from './core/interceptors/error.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     provideHttpClient(
-      withInterceptors([loggingInterceptor])
+      withInterceptors([
+        loggingInterceptor,
+        errorInterceptor
+      ])
     )
   ]
 };
